aa3a69566c00a505353cd95f786ca4c2ea2e8f5b
HeeCheolKim <msn9975@gmail.com>
2020-10-02 23:03:54 +0900

    fix: Input 컴포넌트 기본 스타일 추가

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -20,14 +20,27 @@ const DEFAULT_STYLE = css`
   outline: 0;
   border: 1px solid #3d3d3d;
   border-radius: 2px;
+  box-sizing: border-box;
+  position: relative;
+  width: 100%;
+  min-width: 0;
+  line-height: 1.5;
 
   &::placeholder {
     color: #6b6b6b;
+  }
+  &:disabled {
+    background-color: #e3e3e3;
+    cursor: not-allowed;
+  }
+  &:focus {
+    border-color: #4169e1;
   }
 `
 const SIZE = {
   [InputSize.SMALL]: css`
     padding: 0 7px;
+    font-size: 14px;
   `,
   [InputSize.MEDIUM]: css`
     padding: 4px 11px;
@@ -42,6 +55,7 @@ const SIZE = {
 interface IProps {
   type?: InputType
   size?: InputSize
+  disabled?: boolean
   placeholder?: string
   className?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -50,6 +64,7 @@ interface IProps {
 const Input: React.FC<IProps> = ({
   type = InputType.TEXT,
   size = InputSize.MEDIUM,
+  disabled = false,
   placeholder,
   className,
   onChange,
@@ -60,6 +75,7 @@ const Input: React.FC<IProps> = ({
   return (
     <input
       type={type}
+      disabled={disabled}
       placeholder={placeholder}
       className={className}
       css={[DEFAULT_STYLE, SIZE[size]]}
