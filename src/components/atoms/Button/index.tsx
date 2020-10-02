c4b1d63b02d87df25b295b78bfa9d779ce15c56d
HeeCheolKim <msn9975@gmail.com>
2020-10-02 21:50:16 +0900

    fix: Button 컴포넌트 css-in-js 방식으로 변경

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,7 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 import React from 'react'
-import classNames from 'classnames'
-
-import styles from './style.module.scss'
 
 export enum ButtonType {
   BUTTON = 'button',
@@ -15,26 +14,76 @@ export enum ButtonSize {
   LARGE = 'large',
 }
 
+export enum ButtonTheme {
+  DEFAULT = 'default',
+  PRIMARY = 'primary',
+}
+
 interface IProps {
   className?: string
-  size?: ButtonSize
   type?: ButtonType
+  size?: ButtonSize
+  theme?: ButtonTheme
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const DEFAULT_STYLE = css`
+  outline: 0;
+  border: 0;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  font-size: 14px;
+  border-radius: 2px;
+`
+const SIZE = {
+  [ButtonSize.SMALL]: css`
+    padding: 0 8px;
+    height: 24px;
+    font-size: 14px;
+  `,
+  [ButtonSize.MEDIUM]: css`
+    padding: 4px 15px;
+    height: 32px;
+    font-size: 14px;
+  `,
+  [ButtonSize.LARGE]: css`
+    padding: 8px 15px;
+    height: 40px;
+    font-size: 16px;
+  `,
+}
+const THEME = {
+  [ButtonTheme.DEFAULT]: css`
+    background-color: #fff;
+    border: 1px solid #3d3d3d;
+    color: #3d3d3d;
+  `,
+  [ButtonTheme.PRIMARY]: css`
+    background-color: #5075e3;
+    border: 1px solid #5075e3;
+    color: #fff;
+  `,
 }
 
 const Button: React.FC<IProps> = ({
   type = ButtonType.BUTTON,
   size = ButtonSize.MEDIUM,
-  onClick,
+  theme = ButtonTheme.DEFAULT,
   className,
+  onClick,
   children,
 }) => {
-  const classProps = classNames(styles.default, styles[size], className)
   const event = {
     onClick,
   }
   return (
-    <button type={type} className={classProps} {...event}>
+    <button
+      type={type}
+      {...event}
+      className={className}
+      css={[DEFAULT_STYLE, SIZE[size], THEME[theme]]}
+    >
       {children}
     </button>
   )
